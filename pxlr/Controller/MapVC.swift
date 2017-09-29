a04cc2c1b777154e2220ceb1a17f84642e2951ba
Alexandru Corut <r3dm4n@me.com>
2017-09-29 12:44:38 +0100

    MapVC UI

--- pxlr/Controller/MapVC.swift
+++ pxlr/Controller/MapVC.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        mapView.delegate = self
     }
 
 
+    @IBAction func centerMapButtonPressed(_ sender: Any) {
+    }
 }
 
+extension MapVC: MKMapViewDelegate {
+    
+}
