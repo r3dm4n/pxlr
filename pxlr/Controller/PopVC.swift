c0480d26dc580cb48f730dc068e00fc2dc6f6c45
Alexandru Corut <r3dm4n@me.com>
2017-09-30 10:54:39 +0100

    added PopVC

--- pxlr/Controller/PopVC.swift
+++ pxlr/Controller/PopVC.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class PopVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    var passedImage: UIImage!
+    
+    func initData(forImage image: UIImage) {
+        self.passedImage = image
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popImageView.image = passedImage
+        addDoubleTap()
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self as? UIGestureRecognizerDelegate
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
 
 
 
